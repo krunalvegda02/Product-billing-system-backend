5bba32006abe0e303f09a17ebec1d10c9af38537
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-04-30 22:17:08 +0530

    Base Project Structure Ready

---
+++ src/Database/index.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../Constants/db.constants.js";
+import app from "../app.js";
+
+const connectDB = async () => {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URI}/${DB_NAME}`
+    );
+    app.on("error", () => {
+      console.log("Express Error:", error);
+    });
+
+    console.log(`\n MOngoDB connected !! DB HOST: ${connectionInstance.connection.host}`);
+    // console.log("ConnectionInstance :", connectionInstance.connection);
+    
+  } catch (error) {
+    console.log("Database connectivity error:", error);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
---
+++ src/index.js
@@ -0,0 +1,19 @@
+import dotenv from "dotenv";
+import connectDB from "./Database/index.js";
+import app from "./app.js";
+
+dotenv.config({
+  path: "./.env",
+});
+
+const port = process.env.PORT || 8000;
+
+connectDB()
+  .then(() => {
+    app.listen(port, () => {
+      console.log(`Server is running on port ${port}`);
+    });
+  })
+  .catch((err) => {
+    console.log("MONGODB connnection failed: ", err);
+  });
