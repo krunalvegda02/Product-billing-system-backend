de9a00a516e5b68f29b10e004a8655d0906c27c2
krunalvegda02 <Krunal.vegda02@gmail.com>
2025-05-03 22:54:51 +0530

    .

--- src/Controllers/product.controller.js
+++ src/Controllers/product.controller.js
@@ -1,22 +1,57 @@
-import MESSAGE from "../Constants/message";
-import { Product } from "../Models";
-import { ApiError } from "../Utils/ApiError";
-import { ApiResponse } from "../Utils/ApiResponse";
-import { asyncHandler } from "../Utils/AsyncHandler";
+import MESSAGE from "../Constants/message.js";
+import { Product, Category } from "../Models/index.js";
+import { ApiError } from "../Utils/ApiError.js";
+import { ApiResponse } from "../Utils/ApiResponse.js";
+import { asyncHandler } from "../Utils/AsyncHandler.js";
+import { uploadOnCloudinary } from "../Utils/Cloudinary.js";
 
 const createProduct = asyncHandler(async (req, res) => {
-    // add product to any category during creation
-    const { } = req.body;
+    const { name, description, price, inStock, categoryOfProduct, isDiscountActive, ActiveDiscount } = req.body;
     const thumbnail = req.file;
+    console.log(thumbnail);
 
-    
+
+    // if (
+    //     !name || !description || !price || !inStock ||
+    //     !categoryOfProduct || !Array.isArray(categoryOfProduct) || categoryOfProduct.length === 0 || !thumbnail
+    // ) {
+    //     throw new ApiError(400, MESSAGE.ALL_FIELDS_MUST_REQUIRED);
+    // }
+
+    const uploadedThumbnail = await uploadOnCloudinary(thumbnail.path);
+    if (!uploadedThumbnail?.url) throw new ApiError(500, MESSAGE.UPLOAD_CLOUDINARY_ERROR);
+
+    const product = await Product.create({
+        name,
+        description,
+        price,
+        inStock,
+        thumbnail: uploadedThumbnail.url,
+        categories: categoryOfProduct,
+        isDiscountActive,
+        ActiveDiscount
+    });
+    if (!product) throw new ApiError(500, MESSAGE.PRODUCT_CREATE_FAILED);
+
+    // addding peoducts to category products array
+    await Category.updateMany(
+        { _id: { $in: categoryOfProduct } },
+        { $push: { products: product._id } }
+    );
+
+    return res
+        .status(200)
+        .json(new ApiResponse(200, product, MESSAGE.PRODUCT_CREATE_SUCCESS));
 })
 
 const updateProduct = asyncHandler(async (req, res) => {
-
+    const { name, description, price, inStock, categoryOfProduct, isDiscountActive, ActiveDiscount } = req.body;
+    const thumbnail = req.file;
 })
 
 const deleteProduct = asyncHandler(async (req, res) => {
+    const { id } = req.params;
+
 
 })
 
